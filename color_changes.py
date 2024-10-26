0e71f756a79e035decf0b126217ad9920a5a55c1
Preet Borad <boradpreet@gmail.com>
2024-10-26 14:20:28 +0530

    Project Heading Color File

---
+++ color_changes.py
@@ -0,0 +1,10 @@
+from IPython.display import Markdown, display
+
+def display_conclusion():
+    display(Markdown('<p style="text-align:center; color:red;"><strong><span style="font-size:20px;">______Final_Conclusion_of_Model_Report______</span></strong></p>'))
+
+def abc():
+    display(Markdown('<p style="text-align:center; color:red;"><strong><span style="font-size:20px;">____Report_On_Challenges_Faced___</span></strong></p>'))
+
+def abc1():
+    display(Markdown('<p style="text-align:center; color:red;"><strong><span style="font-size:20px;">____Final_Report_Of_EDA____</span></strong></p>'))
